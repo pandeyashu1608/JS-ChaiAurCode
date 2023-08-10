d5313c83a8e731128fe8846dd98243709d7d60c9
pandeyashu1608 <90791614+pandeyashu1608@users.noreply.github.com>
2023-08-10 17:17:23 +0000

    String methods exercised

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -2,7 +2,41 @@
 
 // 7 types :- String, Number, Boolean, null, Undefined, BigInt and Symbol 
 
+let naam="Rameshwar"
+let age=21
+let tru = true
+let nulll = null
+let undef;
+let nul = ""
+let bint = 1232312312312n
+let id = Symbol(123)
+
+console.table(typeof ([naam,age,tru,nulll,undef,nul,bint,id]))
+
+
 
 // 2. Reference Type(Non-primitive type) 
 
 //Array, Objects and Functions
+
+
+
+//Stack(Primitive) and Heap(Non-primitive)
+
+let myName = "Ashutosh Pandey"
+let anotherName = myName
+anotherName = "Ashu"
+
+console.log(anotherName)
+
+
+let userOne = {
+    email:"ashu@gmail.com",
+    id:2
+}
+
+let userTwo = userOne;
+
+userTwo.email = "ashutosh@google.com"
+
+console.log(userOne.email)
